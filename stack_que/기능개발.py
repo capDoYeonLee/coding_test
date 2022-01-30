ff0ce25a16788851e8e32eb273e54c578a881433
capDoYeonLee <ghtyu21@naver.com>
2022-01-30 23:09:23 +0900

    ing

--- stack_que/기능개발.py
+++ stack_que/기능개발.py
@@ -1,11 +1,3 @@
 def solution(progresses, speeds):
-    while len(speeds) != 0 :
-      if speeds[i] + progresses[i] >= 100:
-        speeds.pop(i)
-        progresses.pop(i)
-        count += 1
-    
-      else:
-        s = [x + y for x,y in zip(s,p)]
-    
+    pass 
     
